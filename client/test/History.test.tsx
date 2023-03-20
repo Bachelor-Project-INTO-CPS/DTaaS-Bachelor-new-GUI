02f8b33431e583d6ff275bd08a3d3efb2fa1c7b2
Burnfarm <mathias.braendgaard@gmail.com>
2023-03-20 16:31:57 +0100

    DUGK-33 Initial test for History/Chart. Add mock folder

---
+++ client/test/History.test.tsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe, expect, it } from '@jest/globals';
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react';
+import * as React from 'react';
+import Chart from '../src/route/History/Chart';
+import { ResizeObserverMock } from './mocks/ResizeObserverMock';
+import 'resize-observer-polyfill';
+
+
+
+describe('Chart', () => {
+  it('renders title', () => {
+    window.ResizeObserver =
+    ResizeObserverMock as unknown as typeof ResizeObserver;
+    render(<Chart />);
+    const titleElement = screen.getByText('Observed Output');
+    expect(titleElement);
+  });
+
+});
