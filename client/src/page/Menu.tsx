87d6918088c44d4a4621921e1da07a4a315caf79
Omar <omarg@live.dk>
2023-03-20 11:45:11 +0100

    DUGK-39 Fixed duplication in Menu

--- client/src/page/Menu.tsx
+++ client/src/page/Menu.tsx
@@ -13,20 +13,22 @@ import MenuItems from './MenuItems';
 
 const drawerWidth = 240;
 
+const transition = ({ theme, open }: { theme: Theme; open: boolean }) =>
+  theme.transitions.create(['width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: open
+      ? theme.transitions.duration.enteringScreen
+      : theme.transitions.duration.leavingScreen,
+  });
+
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
+  transition: transition({ theme, open: true }),
   overflowX: 'hidden',
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
+  transition: transition({ theme, open: false }),
   overflowX: 'hidden',
   width: `calc(${theme.spacing(7)} + 1px)`,
   [theme.breakpoints.up('sm')]: {
