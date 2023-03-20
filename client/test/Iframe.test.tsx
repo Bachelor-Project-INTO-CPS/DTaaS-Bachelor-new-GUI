71f272e01278fbd6c40cd04054d654e4b1a4ff87
Omar <omarg@live.dk>
2023-03-20 08:55:59 +0100

    DUGK-26 Made tests work on iframe. Test coverage added on `yarn test`.

--- client/test/Iframe.test.tsx
+++ client/test/Iframe.test.tsx
@@ -1,3 +1,6 @@
+/*
+ * @jest-environment jsdom
+ */
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { describe, expect, it } from '@jest/globals';
@@ -6,15 +9,15 @@ import Iframe from '../src/components/Iframe';
 describe('Iframe', () => {
   it('renders an iframe element with the correct src and title', () => {
     const { getByTitle } = render(
-      <Iframe url="https://example.com" title="Example" />
+      <Iframe url="https://example.com/" title="Example" />
     );
     const iframe = getByTitle('Example') as HTMLIFrameElement;
-    expect(iframe.src).toBe('https://example.com');
+    expect(iframe.src).toBe('https://example.com/');
   });
 
   it('sets the iframe width to 100%', () => {
     const { getByTitle } = render(
-      <Iframe url="https://example.com" title="Example" />
+      <Iframe url="https://example.com/" title="Example" />
     );
     const iframe = getByTitle('Example') as HTMLIFrameElement;
     expect(iframe.width).toBe('100%');
@@ -22,17 +25,19 @@ describe('Iframe', () => {
 
   it('sets the iframe height to 100% if fullsize prop is not provided', () => {
     const { getByTitle } = render(
-      <Iframe url="https://example.com" title="Example" />
+      <Iframe url="https://example.com/" title="Example" />
     );
     const iframe = getByTitle('Example') as HTMLIFrameElement;
-    expect(iframe.height).toBe('100%');
+    expect(iframe.style.height).toBe('100%');
+    expect(iframe.style.flexGrow).toBe('');
   });
 
   it('sets the iframe height to auto if fullsize prop is provided', () => {
     const { getByTitle } = render(
-      <Iframe url="https://example.com" title="Example" fullsize />
+      <Iframe url="https://example.com/" title="Example" fullsize />
     );
     const iframe = getByTitle('Example') as HTMLIFrameElement;
-    expect(iframe.height).toBe('auto');
+    expect(iframe.style.flexGrow).toBe('1');
+    expect(iframe.style.height).toBe('');
   });
 });
