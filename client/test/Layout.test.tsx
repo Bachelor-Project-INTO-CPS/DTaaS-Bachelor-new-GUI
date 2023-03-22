d680ad2fae9b34e70e8834b30ae44094aee6e358
Omar <omarg@live.dk>
2023-03-23 00:12:32 +0100

    DUGK-30 Started layout test pretty dirty.

---
+++ client/test/Layout.test.tsx
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from '../src/page/Layout';
+
+jest.mock('@mui/material/Toolbar', () => ({
+  __esModule: true,
+  default: () => <div data-testid="toolbar" />,
+}));
+
+jest.mock('../src/page/Footer', () => ({
+  __esModule: true,
+  default: () => <div data-testid="footer" />,
+}));
+
+jest.mock('../src/page/Menu', () => ({
+  __esModule: true,
+  default: () => <div data-testid="menu" />,
+}));
+
+describe('Layout component with one element', () => {
+  it('renders without crashing', () => {
+    render(
+      <Layout>
+        <div />
+      </Layout>,
+      { wrapper: BrowserRouter }
+    );
+    expect(true);
+  });
+
+  it('has a main', () => {
+    render(
+      <Layout>
+        <div />
+      </Layout>,
+      { wrapper: BrowserRouter }
+    );
+    const container = screen.getByRole('main');
+    expect(container).toBeInTheDocument();
+  });
+
+  it('has menu and toolbar for spacing', () => {
+    render(
+      <Layout>
+        <div />
+      </Layout>,
+      { wrapper: BrowserRouter }
+    );
+    const menu = screen.getByTestId('menu');
+    const toolbar = screen.getByTestId('toolbar');
+    expect(menu).toBeInTheDocument();
+    expect(toolbar).toBeInTheDocument();
+  });
+
+  it('has footer', () => {
+    render(
+      <Layout>
+        <div />
+      </Layout>,
+      { wrapper: BrowserRouter }
+    );
+    const footer = screen.getByTestId('footer');
+    expect(footer).toBeInTheDocument();
+  });
+
+  it('has a single child', () => {
+    render(
+      <Layout>
+        <div data-testid="thing" />
+      </Layout>,
+      { wrapper: BrowserRouter }
+    );
+    const div = screen.getByTestId('thing');
+    expect(div).toBeInTheDocument();
+  });
+
+  it('has multiple children (2)', () => {
+    render(
+      <Layout>
+        <div data-testid="thing" />
+        <div data-testid="thing2" />
+      </Layout>,
+      { wrapper: BrowserRouter }
+    );
+    const div = screen.getByTestId('thing');
+    const div2 = screen.getByTestId('thing2');
+    expect(div).toBeInTheDocument();
+    expect(div2).toBeInTheDocument();
+  });
+});
