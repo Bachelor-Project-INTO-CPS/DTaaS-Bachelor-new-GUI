a1dda0b1b8800f9d4ac4755dc132ec9b90c71df9
Omar <omarg@live.dk>
2023-03-22 12:04:50 +0100

    DUGK-32 Added mock for "ResizeObserver" to setup. Don't know it's behavoir, use at own discretion.

--- client/test/Dashboard.test.tsx
+++ client/test/Dashboard.test.tsx
@@ -1,28 +1,10 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable class-methods-use-this */
-import Grid from '@mui/material/Grid';
 import { render } from '@testing-library/react';
 import * as React from 'react';
 import Dashboard from '../src/route/dashboard/Dashboard';
-import 'resize-observer-polyfill';
-
-class ResizeObserverMock {
-  observe() {}
-
-  unobserve() {}
-
-  disconnect() {}
-}
 
 describe('Dashboard', () => {
   it('renders Dashboard without crashing', () => {
-    window.ResizeObserver =
-      ResizeObserverMock as unknown as typeof ResizeObserver;
-    render(
-      <Grid container spacing={3} sx={{ minHeight: '100%' }}>
-        <Dashboard />
-      </Grid>
-    );
+    render(<Dashboard />);
     expect(true);
   });
 });
