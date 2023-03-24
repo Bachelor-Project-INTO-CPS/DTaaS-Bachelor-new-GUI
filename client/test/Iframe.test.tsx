5fdef089265df4965dc55ea7b61c3bab931593ef
Asger <asger_bb@hotmail.com>
2023-03-24 10:53:46 +0100

    fixed another codeclimate issue

--- client/test/Iframe.test.tsx
+++ client/test/Iframe.test.tsx
@@ -3,22 +3,22 @@ import { render, screen } from '@testing-library/react';
 import Iframe from '../src/components/Iframe';
 
 describe('Iframe', () => {
+  var iframe: HTMLIFrameElement;
+
   beforeEach(() => {
     render(<Iframe url="https://example.com/" title="Example" />);
+    iframe = screen.getByTitle('Example') as HTMLIFrameElement;
   });
 
   it('renders an iframe element with the correct src and title', () => {
-    const iframe = screen.getByTitle('Example') as HTMLIFrameElement;
     expect(iframe.src).toBe('https://example.com/');
   });
 
   it('sets the iframe width to 100%', () => {
-    const iframe = screen.getByTitle('Example') as HTMLIFrameElement;
     expect(iframe.width).toBe('100%');
   });
 
   it('sets the iframe height to 100% if fullsize prop is not provided', () => {
-    const iframe = screen.getByTitle('Example') as HTMLIFrameElement;
     expect(iframe.style.height).toBe('100%');
     expect(iframe.style.flexGrow).toBe('');
   });
