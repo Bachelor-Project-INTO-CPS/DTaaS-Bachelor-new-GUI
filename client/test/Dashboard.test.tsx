97f0c9b2ad8447a483c7646a3e21d81880f4b1fa
Asger <asger_bb@hotmail.com>
2023-03-24 09:26:37 +0100

    DUGK-32 Unit test for Dashboard with valid mocks

--- client/test/Dashboard.test.tsx
+++ client/test/Dashboard.test.tsx
@@ -1,10 +1,27 @@
-import { render } from '@testing-library/react';
 import * as React from 'react';
-import Dashboard from '../src/route/dashboard/Dashboard';
+import { render, screen } from '@testing-library/react';
+import Dashboard from 'route/dashboard/Dashboard';
+
+jest.mock('route/history/Chart', () => ({
+  default: () => <div>chart-mock</div>,
+}));
+jest.mock('route/history/RecentRuns', () => ({
+  default: () => <div>recent-runs-mock</div>,
+}));
 
 describe('Dashboard', () => {
-  it('renders Dashboard without crashing', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders Dashboard', () => {
     render(<Dashboard />);
     expect(true);
+  });
+
+  it('renders Chart and RecentRuns components', () => {
+    render(<Dashboard />);
+    expect(screen.queryByText('chart-mock')).toBeInTheDocument();
+    expect(screen.queryByText('recent-runs-mock')).toBeInTheDocument();
   });
 });
