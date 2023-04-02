2629540c74a371f0f05ad1ffe4aa4aa31d7e6ea9
Omar <omarg@live.dk>
2023-04-02 19:08:25 +0200

    DUGK-61 Fixed loop issue in menu test

--- client/test/e2e/Menu.test.ts
+++ client/test/e2e/Menu.test.ts
@@ -26,21 +26,28 @@ test.describe('Header Contents and Navigation Links', () => {
 });
 
 test.describe('Navigation from each link to other links', () => {
+  test.afterAll(async ({ browser, context }) => {
+    await context.close();
+    await browser.close();
+  });
+
   links.forEach((link) => {
-    test(`Navigation from ${link.text} link to other links`, async ({
-      page,
-    }) => {
-      await page.goto(`${link.url}`);
-      /* eslint-disable no-restricted-syntax */
-      for (const linkToClick of links) {
-        if (linkToClick !== link) {
-          /* eslint-disable no-await-in-loop */
+    test.describe(`Navigation from ${link.text} to menu links`, () => {
+      const linksToBeTested = links.filter((linkToTest) => linkToTest !== link);
+
+      linksToBeTested.forEach((linkToTest) => {
+        test(`Navigation from ${link.text} to ${linkToTest.text}`, async ({
+          page,
+        }) => {
+          await page.goto(`${link.url}`);
+
           await page
-            .locator(`div[role="button"]:has-text("${linkToClick.text}")`)
+            .locator(`div[role="button"]:has-text("${linkToTest.text}")`)
             .click();
-          await expect(page).toHaveURL(linkToClick.url);
-        }
-      }
+
+          await expect(page).toHaveURL(linkToTest.url);
+        });
+      });
     });
   });
 });
