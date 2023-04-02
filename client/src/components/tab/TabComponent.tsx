bb8f0048ecba0dc2ca0e34a1e4a6ef8da5e6c30a
Burnfarm <mathias.braendgaard@gmail.com>
2023-04-02 16:01:07 +0200

    DUGK-54 convert to react-tabs

--- client/src/components/tab/TabComponent.tsx
+++ client/src/components/tab/TabComponent.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import TabPanel, { a11yProps } from 'components/tab/subcomponents/TabPanel';
-import { useState, SyntheticEvent } from 'react';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import { Paper } from '@mui/material';
+import TabRender from './subcomponents/TabRender';
+import './react-tabs.css'
 
 type TabDataIndex = number & { readonly __tabDataIndex: unique symbol };
 
@@ -28,56 +26,28 @@ export function createTabData(
 }
 
 function TabComponent(props: { tabs: TabData[] }) {
-  const [activeTab, setSelectedTabIndex] = useState<number>(0);
-
-  const handleTabChange = (
-    _event: SyntheticEvent<Element, Event>,
-    newValue: number
-  ): void => {
-    setSelectedTabIndex(newValue);
-  };
 
   return (
-    <Paper
-      sx={{
-        p: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        ...(props.tabs[activeTab].fullsize === true && { minHeight: '100%' }),
-      }}
-    >
-      <Box
-        sx={{
-          borderBottom: 1,
-          borderColor: 'divider',
-        }}
-      >
-        <Tabs
-          value={activeTab}
-          onChange={handleTabChange}
-          aria-label="basic tabs example"
-          sx={{
-            display: 'flex',
-            flexDirection: 'colum',
-            flexGrow: 1,
-          }}
-        >
+  <Paper sx={{
+    p: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100%' ,
+  }}>
+        <Tabs >
+            <TabList>
           {props.tabs.map((tab) => (
-            <Tab
-              key={tab.index}
-              label={tab.label}
-              {...a11yProps(tab.index)}
-              sx={{ flexGrow: 1 }}
-            />
+            <Tab key={tab.index}>{tab.label}</Tab>
           ))}
+          </TabList>
+          {props.tabs.map((tab) => (
+            <TabPanel key={tab.index}>
+              <TabRender >{tab}</TabRender>
+            </TabPanel>
+             ))}         
         </Tabs>
-      </Box>
-      {props.tabs.map((tab) => (
-        <TabPanel key={tab.index} active={activeTab === tab.index}>
-          {tab}
-        </TabPanel>
-      ))}
     </Paper>
+
   );
 }
 
