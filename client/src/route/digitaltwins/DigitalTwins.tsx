fd3815d4fc9ff89777ebc4ce964d8cd81dd25274
Asger <asger_bb@hotmail.com>
2023-04-17 17:00:44 +0200

    DUGK-67 Removed 'Workflows.tsx' and renamed '...Data.ts'

--- client/src/route/digitaltwins/DigitalTwins.tsx
+++ client/src/route/digitaltwins/DigitalTwins.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react';
 import Layout from 'page/Layout';
-import Workflows from './Workflows';
+import TabComponent, { TabData } from 'components/tab/TabComponent';
+import Iframe from 'components/Iframe';
+import tabs from './DigitalTwinData';
+
+const jupyterURL = window.env.REACT_APP_URL_DT;
+
+const tabData: TabData[] = tabs.map((tab, i) => ({
+  label: tab.label,
+  body: (
+    <>
+      {tab.body}
+      {i === 0 && (
+        <Iframe
+          title={`JupyterLight-Demo-${tab.label}`}
+          url={jupyterURL}
+          fullsize
+        />
+      )}
+    </>
+  ),
+}));
 
 function DTContent() {
   return (
     <Layout>
-      <Workflows />
+      <TabComponent tabs={tabData} />
     </Layout>
   );
 }
