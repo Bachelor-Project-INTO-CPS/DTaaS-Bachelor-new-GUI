f4fedd9fcf273174b43eb350bdd7c409872d8e68
Burnfarm <mathias.braendgaard@gmail.com>
2023-04-28 16:33:32 +0200

    update unit test for SignIn

--- client/test/unitTests/Routes/SignIn.test.tsx
+++ client/test/unitTests/Routes/SignIn.test.tsx
@@ -3,11 +3,14 @@ import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import SignIn from 'route/auth/Signin';
 import AuthContext from 'components/AuthContext';
+import { Provider } from 'react-redux';
+import store from 'store/store'
 
 describe('SignIn', () => {
   it('renders the SignIn form', () => {
     render(
       // eslint-disable-next-line @typescript-eslint/no-empty-function
+      <Provider store={store}>
       <AuthContext.Provider
         value={{
           isLoggedIn: false,
@@ -19,6 +22,7 @@ describe('SignIn', () => {
           <SignIn />
         </Router>
       </AuthContext.Provider>
+      </Provider>
     );
     expect(screen.getByLabelText(/Email Address/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Password/i)).toBeInTheDocument();
