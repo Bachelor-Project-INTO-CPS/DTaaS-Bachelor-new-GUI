4aeca5ed7932369e4b1f3ae8f64f8ec42bfc0932
Burnfarm <mathias.braendgaard@gmail.com>
2023-04-28 16:34:31 +0200

    format

--- client/test/unitTests/Routes/SignIn.test.tsx
+++ client/test/unitTests/Routes/SignIn.test.tsx
@@ -4,24 +4,24 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import SignIn from 'route/auth/Signin';
 import AuthContext from 'components/AuthContext';
 import { Provider } from 'react-redux';
-import store from 'store/store'
+import store from 'store/store';
 
 describe('SignIn', () => {
   it('renders the SignIn form', () => {
     render(
       // eslint-disable-next-line @typescript-eslint/no-empty-function
       <Provider store={store}>
-      <AuthContext.Provider
-        value={{
-          isLoggedIn: false,
-          logIn: () => undefined,
-          logOut: () => undefined,
-        }}
-      >
-        <Router>
-          <SignIn />
-        </Router>
-      </AuthContext.Provider>
+        <AuthContext.Provider
+          value={{
+            isLoggedIn: false,
+            logIn: () => undefined,
+            logOut: () => undefined,
+          }}
+        >
+          <Router>
+            <SignIn />
+          </Router>
+        </AuthContext.Provider>
       </Provider>
     );
     expect(screen.getByLabelText(/Email Address/i)).toBeInTheDocument();
