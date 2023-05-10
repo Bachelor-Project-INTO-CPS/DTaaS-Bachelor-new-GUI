97a51806e0350cb6e60c971c2408145aaee9aa7f
Burnfarm <mathias.braendgaard@gmail.com>
2023-05-10 16:59:19 +0200

    unfinished business

---
+++ client/src/components/cart/CartList.tsx
@@ -0,0 +1,15 @@
+import { Asset } from "models/asset";
+import React from "react";
+
+interface OwnProps{
+    assets: Asset[]
+}
+
+function CartList(props:OwnProps){
+return(<>
+Hellos
+</>)
+}
+
+
+export default CartList;
---
+++ client/src/components/cart/ShoppingCart.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useCart from "store/useCart";
+import CartList from "./CartList";
+
+
+
+function ShoppingCart(){
+    const {state,actions} = useCart(); 
+    const navigate = useNavigate();
+
+
+    return(<>
+    <CartList assets={state.assets}></CartList>
+    <div>
+        <Button variant="contained" onClick={() => actions.clearCart()}>Clear Cart</Button>
+        <Button variant="contained" onClick={() => navigate('/digitaltwins')}>Proceed</Button>
+    </div>
+    
+    </>)
+}
+
+
+export default ShoppingCart;
