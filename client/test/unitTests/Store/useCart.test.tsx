669fa0f0f9e22b9c95b5367a04f4a514cbeec670
Mathias Brændgaard <mathias.braendgaard@gmail.com>
2023-05-10 16:40:59 +0200

    add useCart hook (#57)
    * add useCart hook
    
    * add unit tests for Store/cart
    
    * formatting

---
+++ client/test/unitTests/Store/useCart.test.tsx
@@ -0,0 +1,35 @@
+import { renderHook } from '@testing-library/react';
+import { useSelector } from 'react-redux';
+import useCart from 'store/useCart';
+import {
+  addToCart,
+  removeFromCart,
+  clearCart,
+} from 'store/CartStore/cart.slice';
+
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+}));
+
+describe('useCart', () => {
+  beforeEach(() => {
+    (useSelector as jest.Mock).mockReset();
+  });
+
+  it('should return cart state and actions', () => {
+    const mockCartState = { assets: [{ path: '/assets/1', isFolder: false }] };
+
+    const actions = {
+      addToCart,
+      removeFromCart,
+      clearCart,
+    };
+    const mockSelector = jest.fn().mockReturnValue(mockCartState);
+    (useSelector as jest.Mock).mockImplementation(mockSelector);
+
+    const { result } = renderHook(() => useCart());
+
+    expect(result.current.state).toEqual(mockCartState);
+    expect(result.current.actions).toEqual(actions);
+  });
+});
