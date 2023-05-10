d6d7590094b15271448bf5ca012914b9b37aadcc
Asger <asger_bb@hotmail.com>
2023-05-10 13:37:01 +0200

    DUGK-44 Added library elements for functions

--- client/src/route/library/Library.tsx
+++ client/src/route/library/Library.tsx
@@ -5,19 +5,70 @@ import Iframe from 'components/Iframe';
 import { TabData } from 'components/tab/subcomponents/TabRender';
 import { useURLforLIB } from 'util/envUtil';
 import { Typography } from '@mui/material';
+import { CardData } from 'components/card/CardComponent';
+import CardContainer from 'components/card/CardContainer';
 import tabs from './LibraryTabData';
 
-function LibraryContent() {
+const cardData: CardData[] = [
+  {
+    title: 'fileTest1',
+    description: 'Small description of fileTest1',
+    icon: 'description',
+  },
+  {
+    title: 'folderTest1',
+    description:
+      'Small description of folderTest1 bla bla bla esdrfsd sdf  asd as df sdfas',
+    icon: 'folder',
+  },
+  {
+    title: 'fileTest2',
+    description: 'Small description of fileTest2',
+    icon: 'description',
+  },
+  {
+    title: 'fileTest3',
+    description: 'Small description of fileTest3',
+    icon: 'description',
+  },
+  {
+    title: 'folderTest1',
+    description:
+      'Small description of folderTest1 bla bla bla esdrfsd sdf  asd as df sdfas',
+    icon: 'folder',
+  },
+  {
+    title: 'fileTest3',
+    description: 'Small description of fileTest3',
+    icon: 'description',
+  },
+  {
+    title: 'folderTest1',
+    description:
+      'Small description of folderTest1 bla bla bla esdrfsd sdf  asd as df sdfas',
+    icon: 'folder',
+  },
+];
+
+function useLibraryData() {
   const LIBurl = useURLforLIB();
   const tabsData: TabData[] = tabs.map((tab) => ({
     label: tab.label,
     body: (
       <>
         <Typography variant="body1">{tab.body}</Typography>
-        <Iframe title={`JupyterLight-Demo-${tab.label}`} url={LIBurl} />
+        {tab.label === 'Functions' && <CardContainer data={cardData} />}
+        {tab.label !== 'Functions' && (
+          <Iframe title={`JupyterLight-Demo-${tab.label}`} url={LIBurl} />
+        )}
       </>
     ),
   }));
+  return tabsData;
+}
+
+function LibraryContent() {
+  const tabsData = useLibraryData();
   return (
     <Layout>
       <TabComponent tabs={tabsData} />
