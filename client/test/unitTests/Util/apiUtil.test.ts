37c8cfe14da72f41196b7c5331db3a52efb81671
Omar <omarg@live.dk>
2023-05-13 20:30:54 +0200

    DUGK-73 okay, better testing. Thanks CodeClimate.

--- client/test/unitTests/Util/apiUtil.test.ts
+++ client/test/unitTests/Util/apiUtil.test.ts
@@ -48,70 +48,66 @@ jest.mock('react-relay', () => ({
       : [],
 }));
 
-it('returns assets for given directory path', async () => {
-  fakeAssets = mock3Assets;
-  renderHook(
-    () => {
-      const assets = useAssets(testPath);
-      expect(assets.length).toBe(mock3Assets.length);
-    },
-    { wrapper: initUser }
-  );
-});
+describe('useAssets', () => {
+  it('returns assets for given directory path', async () => {
+    fakeAssets = mock3Assets;
+    renderHook(
+      () => {
+        const assets = useAssets(testPath);
+        expect(assets.length).toBe(mock3Assets.length);
+      },
+      { wrapper: initUser }
+    );
+  });
 
-it('returns empty array if no assets found', async () => {
-  fakeAssets = [];
-  renderHook(
-    () => {
-      const assets = useAssets(testPath);
-      expect(assets.length).toBe(0);
-    },
-    { wrapper: initUser }
-  );
-});
+  it('returns empty array if no assets found', async () => {
+    fakeAssets = [];
+    renderHook(
+      () => {
+        const assets = useAssets(testPath);
+        expect(assets.length).toBe(0);
+      },
+      { wrapper: initUser }
+    );
+  });
 
-it('detects directories', async () => {
-  fakeAssets = mock3Assets;
-  renderHook(
-    () => {
-      const assets = useAssets(testPath);
-      expect(assets[2].isDir).toBe(true);
-    },
-    { wrapper: initUser }
-  );
-});
+  it('detects directories', async () => {
+    fakeAssets = mock3Assets;
+    renderHook(
+      () => {
+        const assets = useAssets(testPath);
+        expect(assets[2].isDir).toBe(true);
+      },
+      { wrapper: initUser }
+    );
+  });
 
-it('detects files', async () => {
-  fakeAssets = mock3Assets;
-  renderHook(
-    () => {
-      const assets = useAssets(testPath);
-      expect(assets[0].isDir).toBe(false); // Not the most robust test, but it works
-    },
-    { wrapper: initUser }
-  );
-});
+  it('detects files', async () => {
+    fakeAssets = mock3Assets;
+    renderHook(
+      () => {
+        const assets = useAssets(testPath);
+        expect(assets[0].isDir).toBe(false); // Not the most robust test, but it works
+      },
+      { wrapper: initUser }
+    );
+  });
 
-it('handles only 1 file', () => {
-  fakeAssets = [mock3Assets[0]];
-  renderHook(
-    () => {
-      const assets = useAssets(testPath);
-      expect(assets.length).toBe(1);
-    },
-    { wrapper: initUser }
-  );
-});
+  it('handles only 1 entry', () => {
+    mock3Assets.forEach((asset) => {
+      fakeAssets = [asset];
+      renderHook(
+        () => {
+          const assets = useAssets(testPath);
+          expect(assets.length).toBe(1);
+          expect(assets[0].name).toBe(asset.name);
+          expect(assets[0].path).toBe(asset.path);
+          expect(assets[0].isDir).toBe(asset.isDir);
+        },
+        { wrapper: initUser }
+      );
+    });
+  });
 
-it('handles only 1 directory', () => {
-  fakeAssets = [mock3Assets[2]];
-  renderHook(
-    () => {
-      const assets = useAssets(testPath);
-      expect(assets.length).toBe(1);
-    },
-    { wrapper: initUser }
-  );
+  // Add more tests here to determin path and username has been set correctly
 });
-
-// Add more tests here to determin path and username has been set correctly
