f880c7773f7f780cceb53983cb56742a06ec5fef
Omarley7 <51030995+Omarley7@users.noreply.github.com>
2023-05-14 20:49:14 +0200

    Redux update (#61)
    * Added setupStore for testability
    
    * Renamed import of slice to reducer
    
    * Using typed store hooks
    
    * Testing with the real Redux store instead of mock.
    Renamed envUtil functions to hooks, as they use storeHook.
    Added documentation for testUtil functions
    
    * Some hooks were actually not hooks
    
    * Updated Asset model
    
    * Add AppAccess hook to Store. Add testing for CartAccess
    
    * Renamed globalMocks to utilMocks
    
    * Everything is now using the new Access classes.
    
    * Fixed infinite loop in AppAccess
    
    * Still needs tests of AppAccess and UserAccess classes
    
    ---------
    
    Co-authored-by: Burnfarm <mathias.braendgaard@gmail.com>

--- client/test/unitTests/__mocks__/util_mocks.ts
+++ client/test/unitTests/__mocks__/util_mocks.ts
@@ -1,19 +1,12 @@
 export const mockURLforDT = 'https://example.com/URL_DT';
 export const mockURLforLIB = 'https://example.com/URL_LIB';
-export const mockURLforWorkbench = 'https://example.com/URL_WORKBENCH';
 
 jest.mock('util/envUtil', () => ({
   useURLforDT: () => mockURLforDT,
   useURLforLIB: () => mockURLforLIB,
-  getURLforWorkbench: () => mockURLforWorkbench,
-  getWorkbenchLinkValues: () => [
+  useWorkbenchLinkValues: () => [
     { key: '1', link: 'link1' },
     { key: '2', link: 'link2' },
     { key: '3', link: 'link3' },
   ],
-}));
-
-jest.mock('react-redux', () => ({
-  useSelector: jest.fn(),
-  useDispatch: () => jest.fn(),
 }));
