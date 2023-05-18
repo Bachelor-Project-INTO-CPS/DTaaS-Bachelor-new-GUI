0871a021039169c01eab084460ae9226160da1f1
Omarley7 <51030995+Omarley7@users.noreply.github.com>
2023-05-18 12:22:23 +0200

    Cart store (#64)
    * unfinished business
    
    * formatting
    
    * DUGK-46 added quick cart list
    
    * Add CartItemRender component
    
    * linting
    
    * move CartItemRender to CartList.tsx file
    
    ---------
    
    Co-authored-by: Burnfarm <mathias.braendgaard@gmail.com>

--- client/test/unitTests/Routes/Library.test.tsx
+++ client/test/unitTests/Routes/Library.test.tsx
@@ -1,18 +1,7 @@
 import * as React from 'react';
 import Library from 'route/library/Library';
 import tabs from 'route/library/LibraryTabData';
-// import { mockURLforLIB } from '../__mocks__/global_mocks';
-import {
-  InitRouteTests,
-  itDisplaysContentOfTabs,
-  // itHasCorrectURLOfTabsWithIframe,
-  // TabLabelURLPair,
-} from '../testUtils';
-
-// const urlsByTabs: TabLabelURLPair[] = tabs.map((tab) => ({
-//   label: tab.label,
-//   url: mockURLforLIB,
-// }));
+import { InitRouteTests, itDisplaysContentOfTabs } from '../testUtils';
 
 describe('Library with no props', () => {
   InitRouteTests(<Library />);
