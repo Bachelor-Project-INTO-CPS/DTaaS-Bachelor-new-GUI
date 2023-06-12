6455237ce94553069f204a5bbc5cdb18c5c3d52a
Omarley7 <omarg@live.dk>
2023-06-12 16:35:07 +0200

    enable trace

--- client/script/test.bash
+++ client/script/test.bash
@@ -9,6 +9,6 @@ jest .
 # { { yarn start 2>&3 || [ $? -eq 137 ]; } 3>&2 2>/dev/null & } <-- Does not work. Trying to only suppress error 137.
 yarn start >/dev/null & # Start server in background. Suppress stderr.
 printf "\n\n################ Running Playwright ################"
-playwright test
+playwright test --trace on
 printf "Closing server on port 4000..."
 npx kill-port 4000
