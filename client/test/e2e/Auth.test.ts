3cfd7e5673aba3cb6244f8bed0c18264560c1eaf
Omarley7 <omarg@live.dk>
2023-06-12 14:28:27 +0200

    Replace locators with better tooling. Removed unused util

--- client/test/e2e/Auth.test.ts
+++ client/test/e2e/Auth.test.ts
@@ -13,28 +13,28 @@ test.describe('Tests on Authentication Flow', () => {
     //    await page.goto(config.baseURL);
     await expect(page).toHaveTitle('The Digital Twin as a Service');
 
-    await page.locator('button:has-text("Sign In")').click();
+    await page.getByRole('button').filter({ hasText: 'Sign In' }).click();
 
     await expect(page).toHaveURL(/.*library/);
   });
 
   test('Fill the signin fields', async ({ page }) => {
-    await page.locator('input[name="username"]').fill('user@au.dk');
-    await page.locator('button').first().click();
+    await page.getByLabel('username').fill('user@au.dk');
+    await page.getByRole('button').first().click();
 
     await expect(page).toHaveURL(/.*library/);
   });
 
   test('Account Button Contents and Links', async ({ page, baseURL }) => {
-    await page.locator('button:has-text("Sign In")').click();
+    await page.getByRole('button').filter({ hasText: 'Sign In' }).click();
     await expect(page).toHaveURL(/.*library/);
 
-    await page.locator('[aria-label="Open settings"]').click();
-    await page.locator('text=Account').click();
+    await page.getByLabel('Open settings').click({ timeout: 200 });
+    await page.getByText('Account').click();
     await expect(page).toHaveURL('./account');
 
-    await page.locator('[aria-label="Open settings"]').click();
-    await page.locator('text=Logout').click();
+    await page.getByLabel('Open settings').click();
+    await page.getByText('Logout').click();
     await expect(page).toHaveURL(baseURL?.replace(/\/$/, '') ?? './');
   });
 
@@ -46,7 +46,9 @@ test.describe('Tests on Authentication Flow', () => {
       await previousPromise;
       await page.goto(link.url);
       await expect(page).toHaveURL(baseURL?.replace(/\/$/, '') ?? './');
-      await expect(page.locator('button:has-text("Sign In")')).toBeVisible();
+      await expect(
+        page.getByRole('button').filter({ hasText: 'Sign In' })
+      ).toBeVisible();
     }, Promise.resolve());
   });
 });
